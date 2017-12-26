65b73a5eb3a641dfbe2b59f64471e52648916779
mikeweishuai <mikews_wwzl@icloud.com>
2017-12-26 22:38:27 +0800

    Randomly generate numbers within 100

---
+++ app/src/main/java/com/example/android/randomnumbergenerater/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.randomnumbergenerater;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void randomize(View view){
+        display((int)(Math.random()*100+1));
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.random_quantity_view);
+        quantityTextView.setText("" + number);
+    }
+}
